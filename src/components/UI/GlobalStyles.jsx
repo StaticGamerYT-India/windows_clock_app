367340d0ab28b0a7eda843094578e855d0e1f827
Smeet Parmar <77852984+StaticGamerYT-India@users.noreply.github.com>
2025-05-10 22:46:09 +0530

    globalstyles

--- src/components/UI/GlobalStyles.jsx
+++ src/components/UI/GlobalStyles.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useTheme } from '../context/ThemeContext';
+import { useTheme } from '../../context/ThemeContext';
 
 /**
  * GlobalStyles component that applies dynamic styles based on theme
